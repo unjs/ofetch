c3a63e2b337b09b082eb9faf8e23e818d866c49c
Pooya Parsa <pyapar@gmail.com>
2020-12-12 19:53:02 +0100

    fix: preserve params when using baseURL

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,7 +7,10 @@ describe('ohmyfetch', () => {
   let listener: Listener
 
   it('setup', async () => {
-    const app = createApp().use('/api', req => (getParams(req.url || '')))
+    const app = createApp()
+      .use('/ok', () => 'ok')
+      .use('/params', req => (getParams(req.url || '')))
+      .use('/url', req => req.url)
     listener = await listen(app)
   })
 
@@ -15,18 +18,19 @@ describe('ohmyfetch', () => {
     await listener.close()
   })
 
-  it('api', async () => {
-    expect(await $fetch('api', {
-      baseURL: listener.url,
-      params: { api: 1 }
-    })).toMatchObject({ api: '1' })
+  it('ok', async () => {
+    expect(await $fetch(listener.getURL('ok'))).toBe('ok')
+  })
+
+  it('baseURL', async () => {
+    expect(await $fetch('/x?foo=123', { baseURL: listener.getURL('url') })).toBe('/x?foo=123')
   })
 
   it('404', async () => {
-    const err: FetchError = await $fetch('404', { baseURL: listener.url }).catch(err => err)
+    const err: FetchError = await $fetch(listener.getURL('404'), { baseURL: listener.url }).catch(err => err)
     expect(err.stack).toMatch('404 Not Found')
     expect(err.data).toMatch('Not Found (404)')
     expect(err.response?.data).toBe(err.data)
-    expect(err.request).toBe('http://localhost:3000/404')
+    expect(err.request).toBe(listener.getURL('404'))
   })
 })

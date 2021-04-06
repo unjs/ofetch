18b3eda3c7c2dd5edf351ee6aee95314744d1868
Pooya Parsa <pyapar@gmail.com>
2021-04-06 13:29:15 +0200

    chore: update test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,12 @@
 import { listen, Listener } from 'listhen'
-import { getQuery } from 'ufo'
+import { getQuery, joinURL } from 'ufo'
 import { createApp, useBody } from 'h3'
 import { Headers } from 'node-fetch'
 import { $fetch, FetchError } from '../src/node'
 
 describe('ohmyfetch', () => {
   let listener: Listener
+  const getURL = (url: string) => joinURL(listener.url, url)
 
   it('setup', async () => {
     const app = createApp()
@@ -21,15 +22,15 @@ describe('ohmyfetch', () => {
   })
 
   it('ok', async () => {
-    expect(await $fetch(listener.getURL('ok'))).toBe('ok')
+    expect(await $fetch(getURL('ok'))).toBe('ok')
   })
 
   it('baseURL', async () => {
-    expect(await $fetch('/x?foo=123', { baseURL: listener.getURL('url') })).toBe('/x?foo=123')
+    expect(await $fetch('/x?foo=123', { baseURL: getURL('url') })).toBe('/x?foo=123')
   })
 
   it('stringifies posts body automatically', async () => {
-    const { body } = await $fetch(listener.getURL('post'), { method: 'POST', body: { num: 42 } })
+    const { body } = await $fetch(getURL('post'), { method: 'POST', body: { num: 42 } })
     expect(body).toEqual({ num: 42 })
 
     const headerFetches = [
@@ -40,16 +41,16 @@ describe('ohmyfetch', () => {
     ]
 
     for (const sentHeaders of headerFetches) {
-      const { headers } = await $fetch(listener.getURL('post'), { method: 'POST', body: { num: 42 }, headers: sentHeaders as any })
+      const { headers } = await $fetch(getURL('post'), { method: 'POST', body: { num: 42 }, headers: sentHeaders as any })
       expect(headers).toEqual(expect.objectContaining({ 'content-type': 'application/json' }))
     }
   })
 
   it('404', async () => {
-    const err: FetchError = await $fetch(listener.getURL('404')).catch(err => err)
+    const err: FetchError = await $fetch(getURL('404')).catch(err => err)
     expect(err.stack).toMatch('404 Not Found')
     expect(err.data).toMatchObject({ stack: [], statusCode: 404, statusMessage: 'Not Found' })
     expect(err.response?.data).toBe(err.data)
-    expect(err.request).toBe(listener.getURL('404'))
+    expect(err.request).toBe(getURL('404'))
   })
 })

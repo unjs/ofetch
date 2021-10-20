5b390bf4cb5cf9286253d11e1cb3593859ef3cd1
Mohammad Saleh Fadaei <54557683+ms-fadaei@users.noreply.github.com>
2021-10-21 00:02:58 +0330

    refactor: throwing error at the end of getGlobal function

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { createFetch } from './base'
 
 export * from './base'
 
+// ref: https://github.com/tc39/proposal-global
 const getGlobal = function () {
   if (typeof globalThis !== 'undefined') { return globalThis }
   if (typeof self !== 'undefined') { return self }
   if (typeof window !== 'undefined') { return window }
   if (typeof global !== 'undefined') { return global }
-  return (function() { return this })();
+  if (typeof this !== 'undefined') { return this }
+  throw new Error('unable to locate global object')
 }
 
 export const fetch = getGlobal().fetch || (() => {

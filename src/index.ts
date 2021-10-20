e0233b7017979bb60cced3c2539f7be71809ef44
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2021-10-21 00:51:55 +0330

    refactor: sooo, just adding globalThis to getGlobal

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import { createFetch } from './base'
 
 export * from './base'
 
-// ref: https://mathiasbynens.be/notes/globalthis
-const getGlobal = () => {
+// ref: https://github.com/tc39/proposal-global
+const getGlobal = function () {
   if (typeof globalThis !== 'undefined') { return globalThis }
   if (typeof self !== 'undefined') { return self }
   if (typeof window !== 'undefined') { return window }
   if (typeof global !== 'undefined') { return global }
-  if (typeof this !== 'undefined') { return this }
   throw new Error('unable to locate global object')
 }
 

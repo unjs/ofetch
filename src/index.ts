b5c0c3bb38289a83164570ffa7458c3f47c6d41b
Mohammad Saleh Fadaei <54557683+ms-fadaei@users.noreply.github.com>
2021-10-22 18:25:03 +0330

    fix: check for `globalThis` before fallback to shims (#20)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './base'
 
 // ref: https://github.com/tc39/proposal-global
 const getGlobal = function () {
+  if (typeof globalThis !== 'undefined') { return globalThis }
   if (typeof self !== 'undefined') { return self }
   if (typeof window !== 'undefined') { return window }
   if (typeof global !== 'undefined') { return global }

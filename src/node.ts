54b779b97a6722542bdfe4b0f6dd9e82e59a7010
Pooya Parsa <pyapar@gmail.com>
2021-11-04 21:06:08 +0100

    feat(node): pick `globalThis.fetch` when available over `node-fetch`

--- src/node.ts
+++ src/node.ts
@@ -3,6 +3,6 @@ import { createFetch } from './base'
 
 export * from './base'
 
-export const fetch = nodeFetch.default as any as typeof globalThis.fetch
+export const fetch = globalThis.fetch || nodeFetch.default as any as typeof globalThis.fetch
 
 export const $fetch = createFetch({ fetch })

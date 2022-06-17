0f71a16fd015852cf1db3a71478803cb2f0ba6f4
Johann Schopplich <mail@johannschopplich.com>
2022-06-17 14:38:36 +0200

    fix(builder): custom headers merging

--- src/builder.ts
+++ src/builder.ts
@@ -50,8 +50,8 @@ export function createClient<R extends ResponseType = 'json'> (
             ...(globalOptions as FetchOptions<R>),
             ...opts,
             headers: {
-              ...(headersToObject(globalOptions.headers) || {}),
-              ...(headersToObject(opts.headers) || {})
+              ...headersToObject(globalOptions.headers || {}),
+              ...headersToObject(opts.headers || {})
             },
             method
           })

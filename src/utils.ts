a58c8c5f258e012e092cb5c3299295f4769316a9
Pooya Parsa <pooya@pi0.io>
2022-09-19 13:11:12 +0200

    revert auto detection as todo

--- src/utils.ts
+++ src/utils.ts
@@ -53,9 +53,10 @@ export function detectResponseType (_contentType = ''): ResponseType {
     return 'json'
   }
 
-  if (contentType === 'application/octet-stream') {
-    return 'stream'
-  }
+  // TODO
+  // if (contentType === 'application/octet-stream') {
+  //   return 'stream'
+  // }
 
   if (textTypes.has(contentType) || contentType.startsWith('text/')) {
     return 'text'

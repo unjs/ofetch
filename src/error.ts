784a7c0524a60406b0ba09055502107ef57ef5c9
Daniel Roe <daniel@roe.dev>
2022-10-27 11:08:33 +0200

    feat: add `status` and `statusText` to fetch errors (#152)

--- src/error.ts
+++ src/error.ts
@@ -5,6 +5,10 @@ export class FetchError<T = any> extends Error {
   request?: FetchRequest
   response?: FetchResponse<T>
   data?: T
+  status?: number
+  statusText?: string
+  statusCode?: number
+  statusMessage?: string
 }
 
 export function createFetchError<T = any> (request: FetchRequest, error?: Error, response?: FetchResponse<T>): FetchError<T> {
@@ -21,6 +25,10 @@ export function createFetchError<T = any> (request: FetchRequest, error?: Error,
   Object.defineProperty(fetchError, 'request', { get () { return request } })
   Object.defineProperty(fetchError, 'response', { get () { return response } })
   Object.defineProperty(fetchError, 'data', { get () { return response && response._data } })
+  Object.defineProperty(fetchError, 'status', { get () { return response && response.status } })
+  Object.defineProperty(fetchError, 'statusText', { get () { return response && response.statusText } })
+  Object.defineProperty(fetchError, 'statusCode', { get () { return response && response.status } })
+  Object.defineProperty(fetchError, 'statusMessage', { get () { return response && response.statusText } })
 
   return fetchError
 }

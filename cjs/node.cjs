3701a1b5a09932b8111a665c951bebf3523ff447
Nozomu Ikuta <16436160+NozomuIkuta@users.noreply.github.com>
2023-01-30 11:18:47 +0900

    fix: fix exports for node

--- cjs/node.cjs
+++ cjs/node.cjs
@@ -2,7 +2,13 @@ const getExport = name => import("../dist/node.mjs").then(r => r[name]);
 const createCaller = name => (input, init) => getExport(name).then(function_ => function_(input, init));
 
 exports.fetch = createCaller("fetch");
+
 exports.ofetch = createCaller("ofetch");
+exports.ofetch.create = defaultOptions => getExport("ofetch").then($fetch => $fetch.create(defaultOptions));
+exports.ofetch.raw = (input, init) => getExport("ofetch").then($fetch => $fetch.raw(input, init));
+exports.ofetch.native = (input, init) => getExport("ofetch").then($fetch => $fetch.native(input, init));
+
 exports.$fetch = createCaller("$fetch");
+exports.$fetch.create = defaultOptions => getExport("$fetch").then($fetch => $fetch.create(defaultOptions));
 exports.$fetch.raw = (input, init) => getExport("$fetch").then($fetch => $fetch.raw(input, init));
 exports.$fetch.native = (input, init) => getExport("$fetch").then($fetch => $fetch.native(input, init));

03627065b270ad4d1fbd1746f53653b918aec0a7
Pooya Parsa <pooya@pi0.io>
2023-02-16 20:00:55 +0100

    build: use standalone CommonJS dist

--- cjs/index.cjs
+++
@@ -1,8 +0,0 @@
-const getExport = name => import("../dist/index.mjs").then(r => r[name]);
-const createCaller = name => (input, init) => getExport(name).then(function_ => function_(input, init));
-
-exports.fetch = createCaller("fetch");
-exports.ofetch = createCaller("ofetch");
-exports.$fetch = createCaller("$fetch");
-exports.$fetch.raw = (input, init) => getExport("$fetch").then($fetch => $fetch.raw(input, init));
-exports.$fetch.native = (input, init) => getExport("$fetch").then($fetch => $fetch.native(input, init));
